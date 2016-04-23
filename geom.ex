259a8ffded6a90f28074c848b739916db932116a
Brion Emde <brione2001@gmail.com>
2016-04-23 13:31:04 -0700

    Etude 2-3: Added docs

--- geom.ex
+++ geom.ex
@@ -1,6 +1,18 @@
 defmodule Geom do
+  @moduledoc """
+  The Geom module knows about geometry.
 
-  def area(width, height) do
+  It could be useful. It isn't.
+  """
+
+  @doc """
+  Computes the area of the rectangle whose width and height are specified.
+
+  Both input parameters default to 1.
+
+  Returns the computed area. 
+  """
+  def area(width \\ 1, height \\ 1) do
     width * height
   end
 end
