208c96d12060aeecdb2b0cb5e122a8ffefee6450
Brion Emde <brione2001@gmail.com>
2016-04-26 11:25:42 -0700

    Etude 6-3 : Lists of Lists

---
+++ teeth.ex
@@ -0,0 +1,37 @@
+defmodule(Teeth) do
+
+  def pocket_depths do
+    [[0], [2,2,1,2,2,1], [3,1,2,3,2,3],
+    [3,1,3,2,1,2], [3,2,3,2,2,1], [2,3,1,2,1,1],
+    [3,1,3,2,3,2], [3,3,2,1,3,1], [4,3,3,2,3,3],
+    [3,1,1,3,2,2], [4,3,4,3,2,3], [2,3,1,3,2,2],
+    [1,2,1,1,3,2], [1,2,2,3,2,3], [1,3,2,1,3,3], [0],
+    [3,2,3,1,1,2], [2,2,1,1,3,2], [2,1,1,1,1,2],
+    [3,3,2,1,1,3], [3,1,3,2,3,2], [3,3,1,2,3,3],
+    [1,2,2,3,3,3], [2,2,3,2,3,3], [2,2,2,4,3,4],
+    [3,4,3,3,3,4], [1,1,2,3,1,2], [2,2,3,2,1,3],
+    [3,4,2,4,4,3], [3,3,2,1,2,3], [2,2,2,2,3,3],
+    [3,2,3,2,3,2]]
+  end
+
+  def alert(patient_teeth) do
+    alert_teeth(patient_teeth, 1, [])
+  end
+
+  def alert_teeth([], _, alerts) do
+    Enum.reverse alerts
+  end
+
+  def alert_teeth([h|t], n, alerts) do
+    if has_bad_pockets?(h) do
+      alert_teeth(t, n + 1, [ n | alerts])
+    else
+      alert_teeth(t, n + 1, alerts)
+    end
+  end
+
+  def has_bad_pockets?(pockets) do
+    Stats.maximum(pockets) >= 4
+  end
+
+end
