a82c89cc47da0a334d1dbf931c21596ac1dabc00
Brion Emde <brione2001@gmail.com>
2016-04-27 11:59:04 -0700

    Etude 7-2 : Create structs from a file
    Completed

--- geography.ex
+++ geography.ex
@@ -2,19 +2,31 @@ defmodule(Geography) do
 
   def make_geo_list(name) do
     {:ok, res} = File.open(name, [:read, :utf8])
-    make_geo_list(res, [])
+    list = make_geo_list(res, [])
     File.close(res)
+    list
   end
 
   def make_geo_list(res, result) do
-    case line = File.read(res, :line) do
+    case line = IO.read(res, :line) do
       :eof -> result
       _ ->
         splits = String.split(line, ",")
         case length(splits) do
-          2 -> %Country{ name: splits[0], language: splits[1] }
-          4 -> %City{ name: splits[0], population: splits[1],
-                      latitude: splits[2], longitude: [3]}
+          2 -> 
+            [ name, language ] = splits
+            make_geo_list(res, 
+                [%Country{ name: String.strip(name), 
+                    language: String.strip(language) } | result])
+          4 ->
+            [country | rest] = result
+            [ name, pop, lat, long ] = splits
+            newCountry = %{country | cities: 
+                [ %City{ name: String.strip(name), 
+                      population: String.strip(pop),
+                      latitude: String.strip(lat), 
+                      longitude: String.strip(long) } | country.cities]}
+            make_geo_list(res, [newCountry | rest])
         end
 
     end
