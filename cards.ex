fdf416eb7f3d0e67c3f3bd16968ddbc58e9dfc9b
Brion Emde <brione2001@gmail.com>
2016-04-28 14:35:59 -0700

    Etude 8-5 : playing around with cards

--- cards.ex
+++ cards.ex
@@ -6,4 +6,39 @@ defmodule(Cards) do
     for r <- ranks, s <- suits, do: { r, s } 
   end
 
+  def shuffle(deck) do
+    :random.seed(:erlang.timestamp)
+    shuffle(deck, [])
+  end
+
+  defp shuffle([], shuffled) do
+    shuffled
+  end
+
+  defp shuffle(deck, shuffled) do
+    index = :random.uniform(length(deck)) - 1
+    next_shuffled = [ Enum.at(deck, index) | shuffled ]
+    new_deck = List.delete_at(deck, index)
+    shuffle(new_deck, next_shuffled) 
+  end
+
+  def deal_bridge_hands() do
+    deck = make_deck |> shuffle
+    deal_bridge_hands(deck, { [[],[],[],[]], 0 })
+  end
+
+  defp deal_bridge_hands([], { hands, _index} ) do
+    hands
+  end
+
+  defp deal_bridge_hands([card | deck], { hands, index } ) do
+    new_hand = [ card | Enum.at(hands, index) ]
+    new_hands = List.replace_at(hands, index, new_hand)
+    new_index = case index do
+      3 -> 0
+      _ -> index + 1
+    end
+    deal_bridge_hands(deck, { new_hands, new_index })
+  end
+
 end
