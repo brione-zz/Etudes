5a2a545557841053a29ffaf823d42e2684d0f67c
Brion Emde <brione2001@gmail.com>
2016-04-30 12:28:33 -0700

    Etude 9-1 : Working two-process war. Still need to convert the game to a process.

--- war.ex
+++ war.ex
@@ -5,79 +5,73 @@ defmodule(War) do
     { p1, p2 } = Cards.deal(deck, 26)
     player1 = spawn(__MODULE__, :player, [ p1 ])
     player2 = spawn(__MODULE__, :player, [ p2 ])
-    IO.puts "Telling #{inspect player1} to send cards"
-    send player1, { :send_cards, self() }
-    IO.puts "Telling #{inspect player2} to send cards"
-    send player2, { :send_cards, self() }
-    game_loop(player1, player2, {[], []})
+    play_a_round(player1, player2, [])
+    send player1, :done
+    send player2, :done
     :ok
   end
 
-  def game_loop(player1, player2, { play_list, pot }) do
-    receive do
-      { :cards, cards, pid } ->
-        IO.puts "Got #{inspect cards} from #{inspect pid}"
-        play_list = [ { cards, pid } | play_list ]
-        if (length(play_list) == 2) do
-          [{handa, pida}, {handb, pidb}] = play_list
-          if length(handa) == 0 do
-            IO.puts "Winner is #{inspect pidb}. Informing him"
-            send pidb, {:receive_cards, pot }
-    IO.puts "Telling #{inspect player1} to send cards"
-    send player1, { :send_cards, self() }
-    IO.puts "Telling #{inspect player2} to send cards"
-    send player2, { :send_cards, self() }
-            game_loop(player1, player2, { [], [] })
-          end
-          if length(handb) == 0 do
-            IO.puts "Winner is #{inspect pida}. Informing him"
-            send pida, {:receive_cards, pot }
-    IO.puts "Telling #{inspect player1} to send cards"
-    send player1, { :send_cards, self() }
-    IO.puts "Telling #{inspect player2} to send cards"
-    send player2, { :send_cards, self() }
-            game_loop(player1, player2, { [], [] })
-          end
-          diff = Cards.rank(hd handa) - Cards.rank(hd handb)
-          cond do
-            diff == 0 ->
-    IO.puts "Telling #{inspect player1} to send cards"
-    send player1, { :send_cards, self() }
-    IO.puts "Telling #{inspect player2} to send cards"
-    send player2, { :send_cards, self() }
-              game_loop(player1, player2, { [], handa ++ handb ++ pot })
-            diff > 0 -> # the first pida wins, whoever that is
-              IO.puts "Winner is #{inspect pida}. Informing him"
-              send pida, {:receive_cards, pot }
-    IO.puts "Telling #{inspect player1} to send cards"
-    send player1, { :send_cards, self() }
-    IO.puts "Telling #{inspect player2} to send cards"
-    send player2, { :send_cards, self() }
-              game_loop(player1, player2, { [], [] })
-            diff < 0 -> # pidb wins
-              IO.puts "Winner is #{inspect pidb}. Informing him"
-              send pidb, {:receive_cards, pot }
-    IO.puts "Telling #{inspect player1} to send cards"
-    send player1, { :send_cards, self() }
-    IO.puts "Telling #{inspect player2} to send cards"
-    send player2, { :send_cards, self() }
-              game_loop(player1, player2, { [], [] })
-          end
-        else
-          game_loop(player1, player2, { play_list, pot })
-        end
-      { :done, pid } ->
-        IO.puts "Got :done from #{inspect pid}"
-        if length play_list == 1 do
-          :exiting
-        else
-          game_loop(player1, player2, { [ pid ], [] })
-        end 
+  def play_a_round(player1, player2, pot) do
+    request_cards_from_players(player1, player2)
+    round_list = receive_cards_from_players([])
+    case evaluate_round_list(round_list) do
+      { :tie, new_pot } -> play_a_round(player1, player2, new_pot)
+      { :win, pid, new_pot } ->
+        send pid, { :receive_cards, new_pot ++ pot }
+        play_a_round(player1, player2, [])
+      { :game_over, pid, new_pot } ->
+        send pid, { :receive_cards, new_pot ++ pot }
+        send pid, :winner
     end
   end
 
+  def request_cards_from_players(player1, player2) do
+    # IO.puts "Telling #{inspect player1} to send cards"
+    send player1, { :send_cards, self() }
+    # IO.puts "Telling #{inspect player2} to send cards"
+    send player2, { :send_cards, self() }
+  end
+
+  def receive_cards_from_players(round_list) do
+    receive do
+      { :cards, cards, pid } ->
+        # IO.puts "Got #{inspect cards} from #{inspect pid}"
+        round_list = [ { cards, pid } | round_list ]
+        if (length(round_list) == 1) do
+          receive_cards_from_players(round_list)
+        else
+          round_list
+        end
+    end    
+  end  
+
+  def get_top_card(cards) do
+    case(cards) do
+      [] -> :no_card
+      [h|_] -> h
+    end
+  end
+
+  def evaluate_round_list([respa, respb]) do
+    {cardsa, pida} = respa
+    {cardsb, pidb} = respb
+    pot = cardsa ++ cardsb
+    tct = { get_top_card(cardsa), get_top_card(cardsb) }
+    case tct do
+      { :no_card, _ } -> { :game_over, pidb, pot }
+      { _, :no_card } -> { :game_over, pida, pot }
+      { ca, cb } ->
+        diff = Cards.rank(ca) - Cards.rank(cb)
+        cond do
+          diff == 0 -> { :tie, pot }
+          diff > 0 -> { :win, pida, pot }
+          diff < 0 -> { :win, pidb, pot }
+        end
+    end    
+  end
+
   def player(hand) do
-    IO.puts("Player #{ inspect self() } got hand of #{ inspect hand }")
+    IO.puts("Player #{ inspect self() } initial hand #{ inspect hand }")
     player_loop(hand)
   end
 
@@ -85,15 +79,17 @@ defmodule(War) do
     receive do
       { :send_cards, pid } ->
         { cards, new_hand } = Cards.deal(hand, 3)
-        IO.puts "#{inspect self()} sending #{inspect cards}"
+        IO.puts "#{inspect self()} new hand #{inspect new_hand}"
         send pid, { :cards, cards, self() }
         player_loop(new_hand)
       { :receive_cards, cards } ->
-        IO.puts "#{inspect self()} received #{inspect cards}"
+        IO.puts "#{inspect self()} new hand #{inspect hand++cards}"
         player_loop(hand ++ cards)
-      { :done, pid } ->
+      :done ->
         IO.puts("Player #{ inspect self() } exiting")
-        send pid, {:done, self()}
+      :winner ->
+        IO.puts("Whoop whoop, I'm a winner!")
+        player_loop(hand)
     end
   end
 
