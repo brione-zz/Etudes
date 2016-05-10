955a99f737b48852dec29c66ace8b6b9e89aa182
Brion Emde <brione2001@gmail.com>
2016-05-10 09:05:53 -0700

    Etude 10-2 Do a better job in fulfilling this exercise.
    learn about the erlang error logger

--- bank.ex
+++ bank.ex
@@ -1,61 +1,62 @@
 defmodule Bank do
 
   def account(balance) do
-    account_transaction(balance)
-
-  end
-
-  def account_transaction(balance) do
     trans = IO.gets("D\)eposit, W\)ithdraw, B\)alance, Q\)quit: ")
+    |> String.upcase
     |> String.first
-    |> code_to_transaction
-    case trans do
-      :withdrawal ->
-        process_withdrawal(balance)
-      :deposit ->
-        process_deposit(balance)
-      :balance ->
-        IO.puts("Balance inquiry #{balance}")
-        account_transaction(balance)
-      :quit ->
+    if trans != "Q" do
+      new_balance = transaction(trans, balance)
+      account(new_balance)
+    else
         IO.puts("Quitting")
     end
   end
 
-  def code_to_transaction(code) do
-    case String.upcase(code) do
-      "D" -> :deposit
-      "W" -> :withdrawal
-      "B" -> :balance
-      "Q" -> :quit
-      _ -> :unkown
-    end
-  end
-
-  def process_withdrawal(balance) do
-    amount = AskArea.get_number("Amount to withdraw: ")
-    cond do
-      amount < 0.0 ->
-        IO.puts("Withdrawal amount is negative: #{amount}")
-        account_transaction(balance)
-      amount > balance ->
-        IO.puts("Withdrawal amount greater than balance: #{amount}")
-        account_transaction(balance)
-      true ->
-        IO.puts("Successful withdrawal")
-        account_transaction(balance - amount)
-    end
-  end
-
-  def process_deposit(balance) do
-    amount = AskArea.get_number("Amount to deposit: ")
-    cond do
-      amount < 0.0 ->
-        IO.puts("Deposit amount is negative: #{amount}")
-        account_transaction(balance)
-      true ->
-        IO.puts("Successful deposit")
-        account_transaction(balance + amount)
+  def transaction(code, balance) do
+    case code do
+      "W" ->
+        amount = AskArea.get_number("Amount to withdraw? ")
+        cond do
+          amount < 0.0 ->
+            :error_logger.warning_msg(
+                "Withdrawal amount is negative: $#{amount}\n")
+            new_balance = balance
+          amount > balance ->
+            :error_logger.warning_msg(
+                "Withdrawal amount greater than balance: $#{amount}\n")
+            new_balance = balance
+          true ->
+            :error_logger.info_msg("Successful withdrawal $#{amount}\n")
+            IO.puts("Successful withdrawal of $#{amount}")
+            IO.puts("Your new balance is $#{balance - amount}")
+            new_balance = balance - amount
+        end
+      "D" ->
+        amount = AskArea.get_number("Amount to deposit? ")
+        cond do
+          amount >= 10000 ->
+            :error_logger.warning_msg(
+                "Large deposit: $#{amount}\n")
+            IO.puts("Your deposit of $#{amount} may be subject to hold.")
+            IO.puts("Your new balance is $#{balance + amount}")
+            new_balance = balance + amount
+          amount < 0.0 ->
+            :error_logger.error_msg(
+                "Deposit amount is negative: $#{amount}\n")
+            IO.puts("Deposits may not be less than zero.")
+            new_balance = balance
+          true ->
+            :error_logger.info_msg("Successful deposit of $#{amount}\n")
+            IO.puts("Your new balance is $#{balance + amount}")
+            new_balance = balance + amount
+        end
+      "B" ->
+        :error_logger.info_msg("Balance inquiry $#{balance}\n")
+        IO.puts("Your balance is $#{balance}")
+        new_balance = balance
+      _ ->
+        IO.puts("Unknown command #{code}")
+        new_balance = balance
     end
   end
 
