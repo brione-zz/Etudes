b219ca088e1bb193f8f3b8aa9a2d13a8ce920dd4
Brion Emde <brione2001@gmail.com>
2016-05-11 15:48:55 -0700

    Etude 12-1 : Now we can fetch weather for a station.
    Didn't do the more detailed formatting.
    
    Still need to add the supervisor to finish the big requirements.

--- weather.ex
+++ weather.ex
@@ -20,8 +20,7 @@ defmodule Weather do
   Initialize the server
   """
   def init(_args) do
-    :inets.start
-    {:ok, pid} = :inets.start(:httpc, [{:profile, :brione}])
+    { :inets.start, [] }
   end
 
   @doc """
@@ -37,8 +36,37 @@ defmodule Weather do
   """
   def handle_call(station, _from, state) do
     url = 'http://w1.weather.gov/xml/current_obs/#{station}.xml'
-    user_agent = {'User-Agent', 'Elixir'}
-    {status, data} = :httpc.request(:get, {url, [user_agent]}, :brione)
-    {:reply, {status, data},   state}
+    user_agent = {'User-Agent', 'Elixir(brione2001@gmail.com)'}
+    request = {url, [user_agent]}
+    {:ok, {status,_headers,data}} = :httpc.request(:get, request, [], [])
+    case status do
+      {_, 200, _} -> 
+        xml = List.to_string(data)
+        {:reply, [get_content("location", xml), 
+            get_content("weather", xml),
+            get_content("observation_time_rfc822", xml), 
+            get_content("temperature_string", xml)],  
+            [station|state]}
+      {_,code, _} -> {:reply, {:error, code}, state}
+    end
   end
+
+  @doc """
+  Handle cast calls. This will result in printing the latest stations
+  to the standard output.
+  """
+  def handle_cast(_, state) do
+    IO.inspect(state)
+    {:noreply, state}
+  end
+
+  def get_content(element_name, xml) do
+    {_, pattern} = Regex.compile(
+      "<#{element_name}>([^<]+)</#{element_name}>")
+    result = Regex.run(pattern, xml)
+    case result do
+      [_all, match] -> {element_name, match}
+      nil -> {element_name, nil}
+    end
+  end 
 end
