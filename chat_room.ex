848e642001291a6f5f6497067147444e543decc2
Brion Emde <brione2001@gmail.com>
2016-05-17 15:42:37 -0700

    Move the possibly working chat room modules here for quicker trying out. Then I'll figure out how to make it work in the project context.

--- chat_room.ex
+++ chat_room.ex
@@ -5,32 +5,47 @@ defmodule ChatRoom do
   Start the chat room server, with initial state of an empty list of clients
   """
   def start_link() do
-    {:ok, _pid} = GenServer.start_link(__MODULE__, [], [])
+    GenServer.start_link(__MODULE__, [], [{:name, {:global, Room}}])
   end
 
   @doc """
   Initialize the server
   """
-  def init(_args) do
-
+  def init(user_list) do
+    {:ok, user_list}    
   end
 
   @doc """
   Adds the user name, server name, and pid (which is in the from parameter)
   to the server’s state. Don’t allow a duplicate user name from the same 
-  server. You can use List.keymember?/3 for this.
+  server. You can use List.keymember?/3 for this. The tuple looks like this:
+  {{user, user_server}, pid}
   """
-  def handle_call({:login, user_name, server_name}, from, state) do
-
-
+  def handle_call({:login, user_name, user_server}, 
+      {pid, _refnum}, user_list) do
+    if !List.keymember?(user_list, user = {user_name, user_server}, 0) do
+      {:reply, :ok, [{user, pid}|user_list]}
+    else
+      :error_logger.info_msg(
+        "Duplicate user/server combination: #{user_name}/#{user_server}\n")
+      {:reply, :error, user_list}
+    end
   end
 
   @doc """
   Removes the user from the state list.
   """
-  def handle_call(:logout, from, state) do
-
-
+  def handle_call(:logout, {pid, _refnum}, user_list) do
+    case List.keytake(user_list, pid, 1) do
+      nil ->
+        :error_logger.info_msg( 
+            "No user with pid: #{pid} found\n")
+        {:reply, :error, user_list}
+      {{{user_name, user_server}, pid}, new_user_list} ->
+        :error_logger.info_msg(
+            "User #{user_name} logged out of chat room\n")
+        {:reply, :ok, new_user_list}
+    end
   end
 
   @doc """
@@ -38,18 +53,19 @@ defmodule ChatRoom do
   GenServer.cast/2 to send the message to each user. You may use a 
   process id as the first argument to GenServer.cast/2.
   """
-  def handle_call({:say, text}, from, state) do
-
-
+  def handle_call({:say, text}, {pid, _refnum}, user_list) do
+    sender = List.keyfind(user_list, pid, 1)
+    Enum.each(user_list, fn({_user, pid}) -> 
+        GenServer.cast(pid, {:message, sender, text}) end)
+    {:reply, :ok, user_list}
   end
 
   @doc """
   Returns the list of names and servers for all people currently in 
   the chat room.
   """
-  def handle_call(:users, from, state) do
-
-
+  def handle_call(:users, from, user_list) do
+    {:reply, Enum.map(user_list, fn({user, _pid}) -> user end), user_list}
   end
 
   @doc """
@@ -58,16 +74,14 @@ defmodule ChatRoom do
   the pid of person at node server_name and sending it a :get_profile
   request.
   """
-  def handle_call({:profile, person, server_name}, from, state) do
-
-
+  def handle_call({:profile, person, server}, {pid, _refnum}, user_list) do
+    case List.keyfind(user_list, {person, server}, 0) do
+      nil -> 
+        :error_logger.info_msg("User #{person}@#{server} not found\n")
+        {:reply, :error, user_list}
+      {user, user_pid} ->
+        {:reply, GenServer.call(user_pid, :profile), user_list}
+    end
   end
-
-end
-
-
-defmodule ChatClient do
-  use GenServer
-
 
 end
---
+++ person.ex
@@ -0,0 +1,148 @@
+defmodule Person do
+  use GenServer
+
+  ## Client side API
+
+  @doc """
+  Start a client-side GenServer
+  """
+  def start_link(server_node) do
+    GenServer.start_link(__MODULE__, server_node, name: Chat)
+  end
+
+  @doc """
+  A convenience function to get the name of the chat host node by doing 
+  GenServer.call(Person, :get_chat_node)
+  """
+  def get_chat_node() do
+    GenServer.call(Chat, :get_chat_node)    
+  end
+
+  @doc """
+  Calls the Person server with a {:login, user_name} request. 
+  If the user name is an atom, use atom_to_binary/1 to convert it 
+  to a string.
+  """
+  def login(user_name) do
+    GenServer.call(Chat, {:login, user_name})
+  end
+
+  @doc """
+  Calls the Person server with a :logout request. As you saw in the 
+  description of chatroom, the server uses the process ID to figure 
+  out who should be logged out.
+  """
+  def logout() do
+    GenServer.call(Chat, :logout)
+  end
+
+  @doc """
+  Calls the Person server with a {:say, text} request.
+  """
+  def say(text) do
+    GenServer.call(Chat, {:say, text})
+  end
+
+  @doc """
+  Calls the chat server with a :users request.
+  """
+  def users() do
+    GenServer.call(Room, :users)
+  end
+
+  @doc """
+  Calls the chat server with a {:who, user_name, user_node} request to see 
+  the profile of the given person. (extra credit)
+  """
+  def who(user_name, user_node) do
+    GenServer.call(Room, {:profile, user_name, user_node})
+  end
+
+  @doc """
+  A convenience method that calls the Person server with a 
+  {:set_profile, key, value} request. (extra credit)
+  """
+  def set_profile(key, value) do
+    GenServer.cast(Chat, {:set_profile, key, value})
+  end
+
+  @doc """
+  A convenience method to get the local profile.
+  """
+  def get_profile() do
+    GenServer.call(Chat, :get_profile)
+  end
+
+  ## Server side API
+
+  @doc """
+  Perform local initialization
+  """
+  def init(server_node) do
+    {:ok, %{server: server_node, user_profile: %{}}}
+  end
+
+  @doc """
+  Returns the chat node name that’s stored in the server’s state. 
+  (Almost all of the wrapper functions to be described in the following 
+  section will need the chat node name.)
+  """
+  def handle_call(:get_chat_node, _from, state) do
+    IO.puts "Sending #{state[:server]}"
+    {:reply, state[:server], state}
+  end
+
+  @doc """
+  Forward this request to the chat room server along with the person’s 
+  server node name.
+  """
+  def handle_call({:login, user_name}, _from, state) do
+    {:reply, GenServer.call(Room, {:login, user_name, state[:server]}), state}
+  end
+
+  @doc """
+  Forward this request to the chat room server.
+  """
+  def handle_call(:logout, _from, state) do
+    {:reply, GenServer.call(Room, :logout), state}
+  end
+
+  @doc """
+  Forward this request to the chat room server.
+  """
+  def handle_call({:say, text}, _from, state) do
+    {:reply, GenServer.call(Room, {:say, text})}
+  end
+
+  @doc """
+  Returns the profile that’s stored in the server’s state (extra credit)
+  """
+  def handle_call(:get_profile, _from, state) do
+    {:reply, state[:user_profile], state}
+  end
+
+  @doc """
+  If the profile already contains the key, replace it with the 
+  given value. Otherwise, add the key and value to the profile. 
+  """
+  def handle_cast({:set_profile, key, value}, state) do
+    if Map.has_key?(state[:user_profile], key) do
+      {:noreply, %{state | :user_profile => 
+          Map.update!(state[:user_profile], key, fn(_v) -> value end)}}
+    else 
+      {:noreply, %{state | :user_profile =>
+          Map.put(state[:user_profile], key, value)}}
+    end
+  end
+
+  @doc """
+  Because the chat room server uses GenServer.cast/2 to send messages 
+  to the people in the room, your handle_cast/3 function will receive 
+  messages sent from other users in this form: 
+  {:message, {from_user, from_server}, text}
+  """
+  def handle_cast({:message, {from_user, from_server}, text}, state) do
+    IO.puts("Message #{from_user}@#{from_server}: #{text}")
+    {:noreply, state}
+  end
+end
